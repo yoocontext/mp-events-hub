32929304950da912dd2f0e714015eebb46379f89
wokie <contextyoo@gmail.com>
2025-12-20 18:14:53 +0300

    refactor(seedwork/rmq/event_map): add CreateEventEvent to event_queue_map

--- app/seedwork/infra/rmq/event_map.py
+++ app/seedwork/infra/rmq/event_map.py
@@ -6,14 +6,19 @@ from seedwork.domain.events.auth import (
     ConfirmRegistrationUserEvent,
     RequestResetPasswordEvent,
 )
+from seedwork.domain.events.events import (
+    CreateEventEvent,
+)
 from seedwork.infra.rmq.queues import (
     REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
     CONFIRM_REGISTRATION_USER_EVENT_QUEUE,
     REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE,
+    EVENT_EVENT_CREATE_QUEUE,
 )
 
 event_queue_map: dict[type[DomainEvent], QueueRmq] = {
     RequestedRegistrationUserEvent: REQUESTED_REGISTRATION_USER_EVENT_QUEUE,
     ConfirmRegistrationUserEvent: CONFIRM_REGISTRATION_USER_EVENT_QUEUE,
-    RequestResetPasswordEvent: REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE
+    RequestResetPasswordEvent: REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE,
+    CreateEventEvent: EVENT_EVENT_CREATE_QUEUE,
 }
--- app/seedwork/infra/rmq/queues.py
+++ app/seedwork/infra/rmq/queues.py
@@ -1,3 +1,9 @@
-REQUESTED_REGISTRATION_USER_EVENT_QUEUE: str = "requested-registration.user.auth.event"
-CONFIRM_REGISTRATION_USER_EVENT_QUEUE: str = "confirm-registration.user.auth.event"
-REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE: str = "requested-reset.user.auth.event"
+"""
+auth(bound-context).user(entity).requested-registration(act).event(type)
+"""
+
+REQUESTED_REGISTRATION_USER_EVENT_QUEUE: str = "auth.user.requested-registration.event"
+CONFIRM_REGISTRATION_USER_EVENT_QUEUE: str = "auth.user.confirm-registration.event"
+REQUESTED_RESET_PASSWORD_USER_EVENT_QUEUE: str = "auth.user.requested-reset.event"
+
+EVENT_EVENT_CREATE_QUEUE: str = "event.event.create.event"
